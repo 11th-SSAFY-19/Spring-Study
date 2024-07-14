79f85fc4bd8e404b699feff9e22b119f3f0efd9c
given-dragon <yijy001@gmail.com>
2024-07-15 00:28:46 +0900

    feat : logging filter 추가

--- src/main/java/com/ssafy/study/global/common/filter/RequestResponseLoggingFilter.java
+++ src/main/java/com/ssafy/study/global/common/filter/RequestResponseLoggingFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -15,24 +16,36 @@ public class RequestResponseLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        printStartLog(request);
+        try {
+            printStartLog(request);
+            filterChain.doFilter(request, response);
+            printFinishLog();
+        } finally {
+            MDC.clear();
+        }
 
-        filterChain.doFilter(request, response);
-
-        printFinishLog(request);
-    }
-
-    private static void printFinishLog(HttpServletRequest request) {
-        String logIdAttr = request.getAttribute("logId").toString();
-        long startMillis = (long) request.getAttribute("startMillis");
-        long usedTime = System.currentTimeMillis() - startMillis;
-        log.info("request_id={}, used_time={}ms", logIdAttr, usedTime);
     }
 
     private static void printStartLog(HttpServletRequest request) {
-        String logId = UUID.randomUUID().toString();
-        request.setAttribute("logId", logId);
-        request.setAttribute("startMillis", System.currentTimeMillis());
-        log.info("request_id={}", logId);
+        String traceId = UUID.randomUUID().toString();
+        MDC.put("traceId", traceId);
+        MDC.put("startMillis", String.valueOf(System.currentTimeMillis()));
+
+        // TODO 원본 client의 ip를 얻는법 생각
+        String clientIp = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+        String requestURI = request.getRequestURI();
+        MDC.put("clientIp", clientIp);
+        MDC.put("userAgent", userAgent);
+        MDC.put("requestURI", requestURI);
+
+        log.info("request start");
+    }
+
+    private static void printFinishLog() {
+        long usedTime = System.currentTimeMillis() - Long.parseLong(MDC.get("startMillis"));
+
+        MDC.put("spentTime(ms)", String.valueOf(usedTime));
+        log.info("request end");
     }
 }
